359c5ba41dcb090daabc66a80482f8e968cc1847
Kexin-Wei <wkx611@outlook.com>
2023-07-13 20:20:31 +0800

    todo2: trajectory generated

--- main.cpp
+++ main.cpp
@@ -12,22 +12,56 @@
  * SPDX-License-Identifier: MIT License
  *
  */
-#include <iostream>
 #include <xarm/wrapper/xarm_api.h>
 #include <test_api.h>
 #include <control_traj.h>
+
+#define RUN_CODE 2
+
+#include <fstream>
 // TODO3: move robot using velocity control following a trajectory (sin curve) and write the speed and acceleration to file
 int main()
 {
     const std::string port("192.168.1.154");
-    // testApi(port);
+#if RUN_CODE == 0
+    testApi(port);
 
+#elif RUN_CODE == 1
     // 1. move robot using position control
-    // fp32 translation[6] = {0, 5, 5, 0, 0, 0};
-    // fp32 newSpeed{1};
-    // moveRobot(port, translation, newSpeed);
+    fp32 translation[6] = {0, 5, 5, 0, 0, 0};
+    fp32 newSpeed{1};
+    moveRobot(port, translation, newSpeed);
 
-    // TODO2: move robot following a trajectory
-    
+#elif RUN_CODE == 2
+    // TODO2: move robot following a trajectory, which wroten in the file
+    // read trajectory from file trajectory.txt
+    std::ifstream trajFile("trajectory.txt", std::ios::in);
+    if (!trajFile.is_open())
+    {
+        std::cout << "Error opening file trajectory.txt" << std::endl;
+        return 0;
+    }
+    std::vector<std::vector<float>> traj;
+    std::string line;
+    while (std::getline(trajFile, line))
+    {
+        std::istringstream iss(line);
+        std::vector<float> point;
+        float value;
+        while (iss >> value)
+        {
+            point.push_back(value);
+        }
+        traj.push_back(point);
+    }
+    for (auto point : traj)
+    {
+        for (auto value : point)
+        {
+            printf("%f ", value);
+        }
+        printf("\n");
+    }
+#endif
     return 1;
 }
---
+++ trajectory_generation.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generateTrajectoryWithNoise(er: np.ndarray, ep: np.ndarray, t: float, noise: float = 0.5, A: float = 1, omega: float = 1, phi: float = 0):
+    """
+    return position at time t
+    Args:
+        er (np.ndarray): unit vector of motion direction
+        ep (np.ndarray): displacement vector of the motion origin
+        A (float, optional): attitude of motion distiance. Defaults to 1.
+        t (float, optional): current time of the motion. Defaults to 0.
+        phi (float, optional): phase of sinusoidal motion. Defaults to 0.
+    """
+    assert er.shape == (3,), "er must be a 3D vector"
+    assert ep.shape == (3,), "ep must be a 3D vector"
+    r = A*np.sin(omega*t+phi) + np.random.normal(0, noise, size=t.shape)
+    return r.reshape(-1, 1)*er.reshape(1, -1) + ep.reshape(1, -1)
+
+
+if __name__ == "__main__":
+
+    # ===== Definition of the trajectory =====
+    er = np.array([1, 1, 1])
+    er = er/np.linalg.norm(er)
+    ep = np.array([0, 1, 3])
+    A = 2.5
+    OMEGA = 1
+    PERIOD = 2*np.pi/OMEGA
+    PHI = 30
+    T = 3*PERIOD
+    dT = PERIOD / 20
+    t = np.linspace(0, T, int(T/dT)+1)
+    trajectory = generateTrajectoryWithNoise(er, ep, t,
+                                             noise=0.2, A=A, omega=OMEGA, phi=PHI)
+    print("dt: ", dT)
+    # from mpl_toolkits.mplot3d import Axes3D
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.plot(trajectory[:, 0], trajectory[:, 1], trajectory[:, 2], "*")
+    # plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(311)
+    ax.plot(t, trajectory[:, 0], "*-")
+    ax.set_title("Trajectory")
+    ax.set_ylabel("x")
+    ax.grid()
+    ax = fig.add_subplot(312)
+    ax.plot(t, trajectory[:, 1], "*-")
+    ax.set_ylabel("y")
+    ax.grid()
+    ax = fig.add_subplot(313)
+    ax.plot(t, trajectory[:, 2], "*-")
+    ax.set_ylabel("z")
+    ax.set_xlabel("t")
+    ax.grid()
+    plt.show()
+
+    # save to file
+    np.savetxt("build/trajectory.txt", trajectory, fmt="%.5f", delimiter=" ")
+    np.savetxt("trajectory.txt", trajectory, fmt="%.5f", delimiter=" ")
+
+    print("Done!")
